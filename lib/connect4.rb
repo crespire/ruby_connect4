c2dc038366af45302f66e3b390574f3667607843
crespire <li.simmon@gmail.com>
2021-12-11 19:27:08 -0500

    Start connect4 spec and class

--- lib/connect4.rb
+++ lib/connect4.rb
@@ -1,3 +1,13 @@
 # frozen_string_literal: true
 
 # lib/connect4.rb
+
+require_relative 'board'
+
+class Connect4
+  def initialize(board: Board.new)
+    @board = board
+    @players = []
+    @display = Display.new
+  end
+end
--- spec/connect4_spec.rb
+++ spec/connect4_spec.rb
@@ -2,7 +2,20 @@
 
 # spec/board_spec.rb
 
+=begin
+  Remember, TEST THE INTERFACE.
+  Query messages return some value, don't change anything.
+  Command messages return nothing, change something.
+
+  Sometimes, a message can have elements of both query and command.
+
+  Key is to remember this spec file tests Connect4. So we only test
+  expectations for query messages. ie, Connect4 receives message and
+  returns expected result.
+=end
+
 require_relative '../lib/connect4'
+require_relative '../lib/board'
 
 describe Connect4 do
   describe '#initialize' do
